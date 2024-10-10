44d14a80e90427ed1cabae6e0b0d3a6c4e5467e9
urajesh-hub <rurmails@gmail.com>
2024-10-10 18:25:15 +0530

    Update dbconfig.js

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -6,7 +6,7 @@ const connectDB=async()=>{
 mongoose.set('strictQuery', false); // or true, depending on your needs
 
 // Then connect to your MongoDB
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
